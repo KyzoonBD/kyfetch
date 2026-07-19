ae0a709a193f57db547bd647c11449d91759fbcd
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2026-07-19 11:37:15 +0600

    Set formula sha256 for v0.4.0 release

--- Formula/kyfetch.rb
+++ Formula/kyfetch.rb
@@ -2,7 +2,7 @@ class Kyfetch < Formula
   desc "Simple internal-URL crawler (mini Screaming Frog)"
   homepage "https://github.com/KyzoonBD/kyfetch"
   url "https://github.com/KyzoonBD/kyfetch/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "3a68f432cd2d4762a6806c7a96fdb1871c7e4ab966d6d33f5199ac7f5e923b9c"
   license "MIT"
   head "https://github.com/KyzoonBD/kyfetch.git", branch: "main"
 
